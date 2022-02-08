c9443e3f1b7f2d186b5965474f5d3e06d944f7de
howowi <owiweihow@gmail.com>
2022-02-08 11:33:43 +0800

    updated tf

--- vars.tf
+++ vars.tf
@@ -28,6 +28,7 @@ provider "oci" {
   region          = var.region
   tenancy_ocid    = var.tenancy_ocid
 }
+
 variable "compartment_ocid" {
   description = "Please provide compartment OCID."
   type        = string
@@ -41,19 +42,6 @@ variable "tenancy_ocid" {
 variable "region" {
   description = "Please provide region for the AWX deployment."
   type        = string
-}
-
-data oci_identity_availability_domain export_ZVCk-PHX-AD-1 {
-  compartment_id = var.compartment_ocid
-  ad_number      = "1"
-}
-data oci_identity_availability_domain export_ZVCk-PHX-AD-2 {
-  compartment_id = var.compartment_ocid
-  ad_number      = "2"
-}
-data oci_identity_availability_domain export_ZVCk-PHX-AD-3 {
-  compartment_id = var.compartment_ocid
-  ad_number      = "3"
 }
 
 variable "vcn_cidr_blocks" {
