ae0857fe3395302d9b5554ebcab74ffad7e5c660
Owi Wei How <owiweihow@gmail.com>
2022-02-10 10:25:37 +0800

    test

--- containerengine.tf
+++ containerengine.tf
@@ -47,16 +47,16 @@ resource oci_containerengine_node_pool prod-oke-pool1 {
     nsg_ids = [
     ]
     placement_configs {
-      availability_domain = data.oci_identity_availability_domain.export_ZVCk-PHX-AD-3.name
-      subnet_id           = oci_core_subnet.export_oke-nodesubnet-quick-test_oke_cluster_ry-7bc0980c4-regional.id
+      availability_domain = data.oci_identity_availability_domain.AD-1.name
+      subnet_id           = oci_core_subnet.prod-oke-nodepool-subnet.id
     }
     placement_configs {
-      availability_domain = data.oci_identity_availability_domain.export_ZVCk-PHX-AD-2.name
-      subnet_id           = oci_core_subnet.export_oke-nodesubnet-quick-test_oke_cluster_ry-7bc0980c4-regional.id
+      availability_domain = data.oci_identity_availability_domain.AD-2.name
+      subnet_id           = oci_core_subnet.prod-oke-nodepool-subnet.id
     }
     placement_configs {
-      availability_domain = data.oci_identity_availability_domain.export_ZVCk-PHX-AD-1.name
-      subnet_id           = oci_core_subnet.export_oke-nodesubnet-quick-test_oke_cluster_ry-7bc0980c4-regional.id
+      availability_domain = data.oci_identity_availability_domain.AD-3.name
+      subnet_id           = oci_core_subnet.prod-oke-nodepool-subnet.id
     }
     size = "3"
   }
@@ -68,7 +68,7 @@ resource oci_containerengine_node_pool prod-oke-pool1 {
     ocpus         = var.shape_ocpus
   }
   node_source_details {
-    image_id    = "ocid1.image.oc1.phx.aaaaaaaanq3ltdsl5ij3cuvqimjwt5yctb7eolglzvkbq2u6wypanj5aazga"
+    image_id    = var.image_os_id
     source_type = "IMAGE"
   }
   ssh_public_key = var.ssh_public_key
@@ -108,46 +108,45 @@ resource oci_containerengine_cluster test-oke-cluster {
   vcn_id = oci_core_vcn.test-oke-vcn.id
 }
 
-# resource oci_containerengine_node_pool export_pool1_1 {
-#   cluster_id     = oci_containerengine_cluster.export_prod_oke_cluster_ry.id
-#   compartment_id = var.compartment_ocid
-#   initial_node_labels {
-#     key   = "name"
-#     value = "prod_oke_cluster_ry"
-#   }
-#   kubernetes_version = "v1.20.11"
-#   name               = "pool1"
-#   node_config_details {
-#     nsg_ids = [
-#     ]
-#     placement_configs {
-#       availability_domain = data.oci_identity_availability_domain.export_ZVCk-PHX-AD-3.name
-#       subnet_id           = oci_core_subnet.export_oke-nodesubnet-quick-prod_oke_cluster_ry-21e30b072-regional.id
-#     }
-#     placement_configs {
-#       availability_domain = data.oci_identity_availability_domain.export_ZVCk-PHX-AD-1.name
-#       subnet_id           = oci_core_subnet.export_oke-nodesubnet-quick-prod_oke_cluster_ry-21e30b072-regional.id
-#     }
-#     placement_configs {
-#       availability_domain = data.oci_identity_availability_domain.export_ZVCk-PHX-AD-2.name
-#       subnet_id           = oci_core_subnet.export_oke-nodesubnet-quick-prod_oke_cluster_ry-21e30b072-regional.id
-#     }
-#     size = "3"
-#   }
-#   node_metadata = {
-#   }
-#   node_shape = "VM.Standard.E3.Flex"
-#   node_shape_config {
-#     memory_in_gbs = "128"
-#     ocpus         = "8"
-#   }
-#   node_source_details {
-#     #boot_volume_size_in_gbs = <<Optional value not found in discovery>>
-#     image_id    = "ocid1.image.oc1.phx.aaaaaaaanq3ltdsl5ij3cuvqimjwt5yctb7eolglzvkbq2u6wypanj5aazga"
-#     source_type = "IMAGE"
-#   }
-#   #quantity_per_subnet = <<Optional value not found in discovery>>
-#   #ssh_public_key = <<Optional value not found in discovery>>
-#   #subnet_ids = <<Optional value not found in discovery>>
-# }
+## ----- Test OKE Node Pool ----- ##
+
+resource oci_containerengine_node_pool test-oke-pool1 {
+  cluster_id     = oci_containerengine_cluster.test-oke-cluster.id
+  compartment_id = var.compartment_ocid
+  initial_node_labels {
+    key   = "name"
+    value = "test-oke-cluster"
+  }
+  kubernetes_version = var.kubernetes_version
+  name               = "test-oke-pool1"
+  node_config_details {
+    nsg_ids = [
+    ]
+    placement_configs {
+      availability_domain = data.oci_identity_availability_domain.AD-1.name
+      subnet_id           = oci_core_subnet.test-oke-nodepool-subnet.id
+    }
+    placement_configs {
+      availability_domain = data.oci_identity_availability_domain.AD-2.name
+      subnet_id           = oci_core_subnet.test-oke-nodepool-subnet.id
+    }
+    placement_configs {
+      availability_domain = data.oci_identity_availability_domain.AD-3.name
+      subnet_id           = oci_core_subnet.test-oke-nodepool-subnet.id
+    }
+    size = "3"
+  }
+  node_metadata = {
+  }
+  node_shape = var.node_shape
+  node_shape_config {
+    memory_in_gbs = var.shape_mems
+    ocpus         = var.shape_ocpus
+  }
+  node_source_details {
+    image_id    = var.image_os_id
+    source_type = "IMAGE"
+  }
+  ssh_public_key = var.ssh_public_key
+}
 
--- vars.tf
+++ vars.tf
@@ -94,9 +94,22 @@ variable "shape_mems" {
   default = "128"
 }
 
+variable "image_os_id" {
+  description = "OS Image OCID of the node pool"
+}
+
 data "oci_core_services" "all_services" {
 }
 
-data "availability_domain" "" {
-  
+data oci_identity_availability_domain AD-1 {
+  compartment_id = var.compartment_ocid
+  ad_number      = "1"
+}
+data oci_identity_availability_domain AD-2 {
+  compartment_id = var.compartment_ocid
+  ad_number      = "2"
+}
+data oci_identity_availability_domain AD-3 {
+  compartment_id = var.compartment_ocid
+  ad_number      = "3"
 }
