5f478afd7517ba19ee4219d1362b58546c93e05a
Owi Wei How <owiweihow@gmail.com>
2022-03-11 16:23:41 +0800

    added attribute

--- events.tf
+++ events.tf
@@ -8,7 +8,7 @@ resource oci_events_rule trigger-build-event {
     }
   }
   compartment_id = var.compartment_ocid
-  condition      = "{\"eventType\": \"com.oraclecloud.datascience.createmodel\"}"
+  condition      = "{\"eventType\": \"com.oraclecloud.datascience.createmodel\", \"data\": {\"compartmentId\": \"${var.compartment_ocid}\", \"resourceName\": \"MLOPS_BANK*\"}}"
   description  = "Trigger Build Pipeline when Model is created in Model Catalog"
   display_name = "Trigger Build ML Model"
   is_enabled = "true"
