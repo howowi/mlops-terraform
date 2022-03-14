c320dcf91bf0292f71b2cced403a60f00313ec97
Owi Wei How <owiweihow@gmail.com>
2022-03-14 15:07:02 +0800

    in progress

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,19 +1,104 @@
 ## API Gateway ##
 
-# resource oci_apigateway_gateway test-ml-model-api-gw {
-#   compartment_id = var.compartment_ocid
-#   display_name  = "test-ml-model-api-gw"
-#   endpoint_type = "PUBLIC"
-#   response_cache_details {
-#     #authentication_secret_id = <<Optional value not found in discovery>>
-#     #authentication_secret_version_number = <<Optional value not found in discovery>>
-#     #connect_timeout_in_ms = <<Optional value not found in discovery>>
-#     #is_ssl_enabled = <<Optional value not found in discovery>>
-#     #is_ssl_verify_disabled = <<Optional value not found in discovery>>
-#     #read_timeout_in_ms = <<Optional value not found in discovery>>
-#     #send_timeout_in_ms = <<Optional value not found in discovery>>
-#     #servers = <<Optional value not found in discovery>>
-#     type = "NONE"
-#   }
-#   subnet_id = oci_core_subnet.export_Public-Subnet-datascience-vcn.id
-# }
+resource oci_apigateway_gateway export_prod-ml-model-api-gw {
+  compartment_id = var.compartment_ocid
+  display_name  = "prod-ml-model-api-gw"
+  endpoint_type = "PUBLIC"
+  subnet_id = oci_core_subnet.export_api-gw-public-subnet.id
+}
+
+resource oci_apigateway_gateway export_test-ml-model-api-gw {
+  compartment_id = var.compartment_ocid
+  display_name  = "test-ml-model-api-gw"
+  endpoint_type = "PUBLIC"
+  subnet_id = oci_core_subnet.export_api-gw-public-subnet.id
+}
+
+resource oci_apigateway_deployment export_prod-ml-model {
+  compartment_id = var.compartment_ocid
+  display_name = "prod-ml-model"
+  freeform_tags = {
+  }
+  gateway_id  = oci_apigateway_gateway.export_prod-ml-model-api-gw.id
+  path_prefix = "/v1"
+  specification {
+    logging_policies {
+      execution_log {
+        is_enabled = "true"
+        log_level  = "INFO"
+      }
+    }
+    routes {
+      backend {
+        connect_timeout_in_seconds = "60"
+        is_ssl_verify_disabled  = "true"
+        read_timeout_in_seconds = "10"
+        send_timeout_in_seconds = "10"
+        type = "HTTP_BACKEND"
+        url  = "http://152.70.155.149:8080/predict"
+      }
+      methods = [
+        "ANY",
+      ]
+      path = "/predict"
+    }
+    routes {
+      backend {
+        connect_timeout_in_seconds = "60"
+        is_ssl_verify_disabled  = "true"
+        read_timeout_in_seconds = "10"
+        send_timeout_in_seconds = "10"
+        type = "HTTP_BACKEND"
+        url  = "http://152.70.155.149:8080/health"
+      }
+      methods = [
+        "ANY",
+      ]
+      path = "/health"
+    }
+  }
+}
+
+resource oci_apigateway_deployment export_test-ml-model {
+  compartment_id = var.compartment_ocid
+  display_name = "test-ml-model"
+  gateway_id  = oci_apigateway_gateway.export_test-ml-model-api-gw.id
+  path_prefix = "/v1"
+  specification {
+    logging_policies {
+      execution_log {
+        is_enabled = "true"
+        log_level  = "INFO"
+      }
+    }
+    routes {
+      backend {
+        connect_timeout_in_seconds = "60"
+        is_ssl_verify_disabled  = "true"
+        read_timeout_in_seconds = "10"
+        send_timeout_in_seconds = "10"
+        type = "HTTP_BACKEND"
+        url  = "http://152.70.129.30:8080/predict"
+      }
+      methods = [
+        "ANY",
+      ]
+      path = "/predict"
+    }
+    routes {
+      backend {
+        connect_timeout_in_seconds = "60"
+        is_ssl_verify_disabled  = "true"
+        read_timeout_in_seconds = "10"
+        send_timeout_in_seconds = "10"
+        type = "HTTP_BACKEND"
+        url  = "http://152.70.129.30:8080/health"
+      }
+      methods = [
+        "ANY",
+      ]
+      path = "/health"
+    }
+  }
+}
+
--- vault.tf
+++ vault.tf
@@ -1,1 +1,32 @@
 ## Vault for OCIR Secret ##
+
+resource oci_vault_secret export_secret_1 {
+  compartment_id = var.compartment_ocid
+  key_id = "ocid1.key.oc1.iad.bzqybfaqaahqg.abuwcljr2vxnzb244vaaqh573xoysdfgqdqdthhvpp3wbijjci5b4cevgvyq"
+  secret_content = "<placeholder for missing required attribute>" #Required attribute not found in discovery, placeholder value set to avoid plan failure
+  secret_name    = "container-registry-auth-token"
+  vault_id       = oci_kms_vault.export_container-registry-auth-token.id
+  lifecycle {
+    ignore_changes = [secret_content]
+  }
+}
+
+resource oci_kms_vault export_container-registry-auth-token {
+  compartment_id = var.compartment_ocid
+  display_name = "container-registry-auth-token"
+  vault_type = "DEFAULT"
+}
+
+resource oci_kms_key export_master-key {
+  compartment_id = var.compartment_ocid
+  desired_state = "ENABLED"
+  display_name  = "master-key"
+  key_shape {
+    algorithm = "AES"
+    curve_id  = ""
+    length    = "32"
+  }
+  management_endpoint = "https://bzqybfaqaahqg-management.kms.us-ashburn-1.oraclecloud.com"
+  protection_mode     = "HSM"
+}
+
