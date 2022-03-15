a8f0f50becbd730bcf9d9bbd32d88812ab608a3b
Owi Wei How <owiweihow@gmail.com>
2022-03-15 16:50:32 +0800

    fixed kube initialize dependancy

--- kube_initialize.tf
+++ kube_initialize.tf
@@ -1,50 +1,70 @@
 ## Initialize Test Kubernetes Cluster ##
 
 resource "null_resource" "create_kubeconfig_directory" {
+    depends_on = [oci_containerengine_node_pool.test-oke-pool1]
+    triggers  =  { 
+        always_run = "${timestamp()}" 
+    }
     provisioner "local-exec" {
         command = "mkdir -p $HOME/.kube"
+        interpreter = [ "/bin/bash","-c"]
     }
 }
 
 resource "null_resource" "create_kubeconfig_test" {
+    triggers  =  { 
+        always_run = "${timestamp()}" 
+    }
     depends_on = [null_resource.create_kubeconfig_directory]
     provisioner "local-exec" {
-        command = "oci ce cluster create-kubeconfig --cluster-id ${oci_containerengine_cluster.test-oke-cluster.id} --file $HOME/.kube/config_test --region ${var.region} --token-version 2.0.0  --kube-endpoint PUBLIC_ENDPOINT"
+        command = "oci ce cluster create-kubeconfig --cluster-id ${oci_containerengine_cluster.test-oke-cluster.id} --file $HOME/.kube/config --region ${var.region} --token-version 2.0.0  --kube-endpoint PUBLIC_ENDPOINT"
+        interpreter = [ "/bin/bash","-c"]
     }
 }
 
-resource "null_resource" "set_kubeconfig_env_test" {
+resource "null_resource" "check_context_test" {
+    triggers  =  { 
+        always_run = "${timestamp()}" 
+    }
     depends_on = [null_resource.create_kubeconfig_test]
     provisioner "local-exec" {
-        command = "export KUBECONFIG=$HOME/.kube/config_test"
+        command = "kubectl config current-context"
+        interpreter = [ "/bin/bash","-c"]
     }
 }
 
 resource "null_resource" "create_namespace_test" {
-    depends_on = [null_resource.set_kubeconfig_env_test]
+    depends_on = [null_resource.check_context_test]
     provisioner "local-exec" {
         command = "kubectl create namespace ml-model"
+        interpreter = [ "/bin/bash","-c"]
     }
 }
 
 resource "null_resource" "create_ocir_secret_test" {
-  depends_on = [null_resource.create_namespace_test]
+  depends_on = [null_resource.create_namespace_test, null_resource.check_context_test]
   provisioner "local-exec" {
     command = "kubectl create secret docker-registry ocir-secret --docker-username='${var.ocir_username}' --docker-password='${var.ocir_password}' --docker-server=${var.ocir_url} --docker-email='${var.ocir_email}' -n ml-model"
+    interpreter = [ "/bin/bash","-c"]
   }
 }
 
 resource "null_resource" "deploy_ml_service_test" {
-    depends_on = [null_resource.create_ocir_secret_test]
+    depends_on = [null_resource.create_ocir_secret_test, null_resource.check_context_test]
     provisioner "local-exec" {
         command = "kubectl apply -f deploy/deploy-ml-model.yml"
+        interpreter = [ "/bin/bash","-c"]
     }
 }
 
 resource "null_resource" "get_model_ip_test" {
-    depends_on = [null_resource.deploy_ml_service_test]
+    triggers  =  { 
+        always_run = "${timestamp()}" 
+    }
+    depends_on = [null_resource.deploy_ml_service_test, null_resource.check_context_test]
     provisioner "local-exec" {
         command = "kubectl get svc ml-model-service -n ml-model --no-headers=true | awk -F ' ' '{print $3}' > data/test_model_ip.txt"
+        interpreter = [ "/bin/bash","-c"]
     }
 }
 
@@ -55,52 +75,60 @@ data "local_file" "test_model_ip" {
 
 ## Initialize Prod Kubernetes Cluster ##
 
-# resource "null_resource" "move_test_kubeconfig" {
-#     depends_on = [null_resource.get_model_ip_test]
-#     provisioner "local-exec" {
-#         command = "mv $HOME/.kube/config $HOME/.kube/test_config_backup"
-#     }
-# }
-
 resource "null_resource" "create_kubeconfig_prod" {
-    depends_on = [null_resource.get_model_ip_test]
+    triggers  =  { 
+        always_run = "${timestamp()}" 
+    }
+    depends_on = [null_resource.get_model_ip_test, oci_containerengine_node_pool.prod-oke-pool1]
     provisioner "local-exec" {
-        command = "oci ce cluster create-kubeconfig --cluster-id ${oci_containerengine_cluster.prod-oke-cluster.id} --file $HOME/.kube/config_prod --region ${var.region} --token-version 2.0.0  --kube-endpoint PUBLIC_ENDPOINT"
+        command = "oci ce cluster create-kubeconfig --cluster-id ${oci_containerengine_cluster.prod-oke-cluster.id} --file $HOME/.kube/config --region ${var.region} --token-version 2.0.0  --kube-endpoint PUBLIC_ENDPOINT"
+        interpreter = [ "/bin/bash","-c"]
     }
 }
 
-resource "null_resource" "set_kubeconfig_env_prod" {
+resource "null_resource" "check_context_prod" {
+    triggers  =  { 
+        always_run = "${timestamp()}" 
+    }
     depends_on = [null_resource.create_kubeconfig_prod]
     provisioner "local-exec" {
-        command = "export KUBECONFIG=$HOME/.kube/config_prod"
+        command = "kubectl config current-context"
+        interpreter = [ "/bin/bash","-c"]
     }
 }
 
 resource "null_resource" "create_namespace_prod" {
-    depends_on = [null_resource.set_kubeconfig_env_prod]
+    depends_on = [null_resource.check_context_prod]
     provisioner "local-exec" {
         command = "kubectl create namespace ml-model"
+        interpreter = [ "/bin/bash","-c"]
     }
 }
 
 resource "null_resource" "create_ocir_secret_prod" {
-  depends_on = [null_resource.create_namespace_prod]
+  depends_on = [null_resource.create_namespace_prod, null_resource.check_context_prod]
   provisioner "local-exec" {
     command = "kubectl create secret docker-registry ocir-secret --docker-username='${var.ocir_username}' --docker-password='${var.ocir_password}' --docker-server=${var.ocir_url} --docker-email='${var.ocir_email}' -n ml-model"
+    interpreter = [ "/bin/bash","-c"]
   }
 }
 
 resource "null_resource" "deploy_ml_service_prod" {
-    depends_on = [null_resource.create_ocir_secret_prod]
+    depends_on = [null_resource.create_ocir_secret_prod, null_resource.check_context_prod]
     provisioner "local-exec" {
         command = "kubectl apply -f deploy/deploy-ml-model.yml"
+        interpreter = [ "/bin/bash","-c"]
     }
 }
 
 resource "null_resource" "get_model_ip_prod" {
-    depends_on = [null_resource.deploy_ml_service_prod]
+    triggers  =  { 
+        always_run = "${timestamp()}" 
+    }
+    depends_on = [null_resource.deploy_ml_service_prod, null_resource.check_context_prod]
     provisioner "local-exec" {
         command = "kubectl get svc ml-model-service -n ml-model --no-headers=true | awk -F ' ' '{print $3}' > data/prod_model_ip.txt"
+        interpreter = [ "/bin/bash","-c"]
     }
 }
 
