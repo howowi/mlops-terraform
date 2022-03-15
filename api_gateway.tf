36f60d1713d600e6434448350451b3be4d98fc67
Owi Wei How <owiweihow@gmail.com>
2022-03-15 17:57:55 +0800

    try to fix api

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,5 +1,27 @@
 ## API Gateway ##
 
+resource "null_resource" "check_data_output_test" {
+  depends_on = [null_resource.get_model_ip_test]
+    triggers  =  { 
+        always_run = "${timestamp()}" 
+    }
+    provisioner "local-exec" {
+        command = "cat data/test_model_ip.txt"
+        interpreter = [ "/bin/bash","-c"]
+    }
+}
+
+resource "null_resource" "check_data_output_prod" {
+  depends_on = [null_resource.get_model_ip_prod]
+    triggers  =  { 
+        always_run = "${timestamp()}" 
+    }
+    provisioner "local-exec" {
+        command = "cat data/prod_model_ip.txt"
+        interpreter = [ "/bin/bash","-c"]
+    }
+}
+
 resource oci_apigateway_gateway prod-ml-model-api-gw {
   compartment_id = var.compartment_ocid
   display_name  = "${var.resource_naming_prefix}-prod-ml-model-api-gw"
@@ -15,7 +37,7 @@ resource oci_apigateway_gateway test-ml-model-api-gw {
 }
 
 resource oci_apigateway_deployment prod-ml-model {
-  depends_on = [data.local_file.prod_model_ip]
+  depends_on = [data.local_file.prod_model_ip, null_resource.check_data_output_prod]
   compartment_id = var.compartment_ocid
   display_name = "${var.resource_naming_prefix}-prod-ml-model-api"
   gateway_id  = oci_apigateway_gateway.prod-ml-model-api-gw.id
@@ -59,7 +81,7 @@ resource oci_apigateway_deployment prod-ml-model {
 }
 
 resource oci_apigateway_deployment test-ml-model {
-  depends_on = [data.local_file.test_model_ip]
+  depends_on = [data.local_file.test_model_ip, null_resource.check_data_output_test]
   compartment_id = var.compartment_ocid
   display_name = "${var.resource_naming_prefix}-test-ml-model-api"
   gateway_id  = oci_apigateway_gateway.test-ml-model-api-gw.id
