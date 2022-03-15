9fb7bd851cc5ef47c6d9774b0aa4987c2354822b
Owi Wei How <owiweihow@gmail.com>
2022-03-15 19:31:16 +0800

    added mlops-model container

--- container_reg.tf
+++ container_reg.tf
@@ -11,3 +11,9 @@ resource oci_artifacts_container_repository test_mlmodel_function {
     display_name = "test-ml-model"
     is_public = "false"
 }
+
+resource oci_artifacts_container_repository mlops_model {
+    compartment_id = var.compartment_ocid
+    display_name = "mlops-model"
+    is_public = "false"
+}
