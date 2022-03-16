b537498387680ac5993f2afa5efcdf73b158a8dc
Owi Wei How <owiweihow@gmail.com>
2022-03-16 18:19:11 +0800

    removed terraform folder

--- terraform/step_1/datascience.tf
+++
@@ -1,9 +0,0 @@
-## ----- Data Science Project ----- ##
-resource oci_datascience_project mlops-datascience-proj {
-  compartment_id = var.compartment_ocid
-  description = "Data Science Project for MLOps"
-  display_name = "${var.resource_naming_prefix}-mlops-datascience-proj"
-  freeform_tags = {
-    "MLOpsV1" = "V1"
-  }
-}
