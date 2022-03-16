afee1acb5c144c82b4d91eeab794c9111c02d7e4
Owi Wei How <owiweihow@gmail.com>
2022-03-16 12:23:11 +0800

    created vault

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output test-ml-model-predict-endpoint {
 output devops-code-repo-url {
     value = oci_devops_repository.mlops-code-repo.http_url
 }
+
+output ml-model-container-repo-path {
+    value = "${var.ocir_url}/${data.oci_objectstorage_namespace.os_namespace.namespace}/${oci_artifacts_container_repository.mlops_model.display_name}"
+}
+
+output container-reg-auth-token-id {
+    value = oci_vault_secret.container-registry-auth-token.id
+}
--- vault.tf
+++ vault.tf
@@ -1,32 +1,28 @@
 # ## Vault for OCIR Secret ##
 
-# resource oci_vault_secret export_secret_1 {
-#   compartment_id = var.compartment_ocid
-#   key_id = "ocid1.key.oc1.iad.bzqybfaqaahqg.abuwcljr2vxnzb244vaaqh573xoysdfgqdqdthhvpp3wbijjci5b4cevgvyq"
-#   secret_content = "<placeholder for missing required attribute>" #Required attribute not found in discovery, placeholder value set to avoid plan failure
-#   secret_name    = "container-registry-auth-token"
-#   vault_id       = oci_kms_vault.export_container-registry-auth-token.id
-#   lifecycle {
-#     ignore_changes = [secret_content]
-#   }
-# }
+resource oci_kms_vault ml-ops-kms-vault {
+  compartment_id = var.compartment_ocid
+  display_name = "ml-ops-kms-vault"
+  vault_type = "DEFAULT"
+}
 
-# resource oci_kms_vault export_container-registry-auth-token {
-#   compartment_id = var.compartment_ocid
-#   display_name = "container-registry-auth-token"
-#   vault_type = "DEFAULT"
-# }
+resource oci_kms_key ml-ops-kms-master-key {
+  compartment_id = var.compartment_ocid
+  desired_state = "ENABLED"
+  display_name  = "ml-ops-kms-master-key"
+  key_shape {
+    algorithm = "AES"
+    length    = "32"
+  }
+  management_endpoint = oci_kms_vault.ml-ops-kms-vault.management_endpoint
+  protection_mode     = "HSM"
+}
 
-# resource oci_kms_key export_master-key {
-#   compartment_id = var.compartment_ocid
-#   desired_state = "ENABLED"
-#   display_name  = "master-key"
-#   key_shape {
-#     algorithm = "AES"
-#     curve_id  = ""
-#     length    = "32"
-#   }
-#   management_endpoint = "https://bzqybfaqaahqg-management.kms.us-ashburn-1.oraclecloud.com"
-#   protection_mode     = "HSM"
-# }
+resource oci_vault_secret container-registry-auth-token {
+  compartment_id = var.compartment_ocid
+  key_id = oci_kms_key.ml-ops-kms-master-key.id
+  vault_id       = oci_kms_vault.ml-ops-kms-vault.id
+  secret_content  = var.ocir_password
+  secret_name    = "container-registry-auth-token"
+}
 
